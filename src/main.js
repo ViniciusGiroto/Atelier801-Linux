eb14c7bc49fa3f9e8aceb388878cd24cd2aa1576
Vinicius G <vinicius@debian>
2018-08-22 23:07:52 -0300

    Lot of changes

---
+++ src/main.js
@@ -0,0 +1,190 @@
+// Load require apps
+const electron = require('electron');
+const path = require('path');
+const flash = require('./modules/flash');
+
+const { app, BrowserWindow, ipcMain, globalShortcut, shell, dialog } = electron;
+
+const libPath = (process.arch === 'x64') ? 'lib64' : 'lib32';
+const flashPath = path.join(__dirname, libPath, 'pepflashplayer');
+
+const winBase = {
+  width: 800,
+  height: 600,
+  show: false,
+};
+
+const games = {
+  tfm: {
+    title: 'Transformice',
+    url: 'http://www.transformice.com/TransformiceChargeur.swf'
+  },
+  neko: {
+    title: 'Nekodancer',
+    url: 'http://www.nekodancer.com/ChargeurNekodancer.swf',
+  },
+  fort: {
+    title: 'Fortoresse',
+    url: 'http://www.fortoresse.com/ChargeurFortoresse.swf'
+  },
+  bbm: {
+    title: 'Bouboum',
+    url: 'http://www.bouboum.com/ChargeurBouboum.swf'
+  },
+  ddm: {
+    title: 'Dead Maze',
+    url: 'http://deadmaze.com/alpha/'
+  },
+  ext: {
+    title: 'Extinction',
+    url: 'http://www.extinction.fr/MiniJeux.swf'
+  },
+};
+
+let flashVersion = '0.0.0.0';
+let win;
+let currentGame = null;
+let hasFlash = false;
+
+global.env = {
+  verbose: false,
+};
+
+// Read arguments
+for (const arg of process.argv) {
+  switch (arg) {
+    case '-v':
+    case '--verbose':
+      env.verbose = true;
+  }
+}
+
+function setupShortcuts() {
+  env.verbose && console.log('Setting up shortcuts');
+  globalShortcut.register('Ctrl+q', () => win.close());
+  globalShortcut.register('Ctrl+Shift+t', () => openGame('tfm'));
+  globalShortcut.register('Ctrl+Shift+n', () => openGame('neko'));
+  globalShortcut.register('Ctrl+Shift+f', () => openGame('fort'));
+  globalShortcut.register('Ctrl+Shift+b', () => openGame('bbm'));
+  globalShortcut.register('Ctrl+Shift+d', () => openGame('ddm'));
+  globalShortcut.register('F11', () => win.setFullScreen(!win.isFullScreen()));
+  globalShortcut.register('Ctrl+Esc', () => (currentGame && openMain()));
+}
+
+function createWindow(config, url) {
+  env.verbose && console.log('Creating new window (%s)', url);
+  const win = new BrowserWindow(config);
+
+  win.setMenu(null);
+  win.loadURL(url);
+
+  win.once('ready-to-show', () => win.show());
+
+  win.webContents.on('will-navigate', (e, url) => {
+    e.preventDefault();
+    shell.openExternal(url);
+  });
+
+  return win;
+}
+
+function openGame(id) {
+  env.verbose && console.log('Opening game (%s)', id);
+  const game = games[id];
+  const newWin = createWindow({
+    ...winBase,
+    title: game.title,
+    icon: path.join(__dirname, 'images', `icon-${id}.png`),
+    nodeIntegration: false,
+    webPreferences: {
+      devTools: false,
+      plugins: true,
+      sandbox: true
+    }
+  }, game.url);
+
+  win && win.close();
+  win = newWin;
+  currentGame = id;
+}
+
+function openMain() {
+  env.verbose && console.log('Opening main window');
+  const newWin = createWindow({
+    ...winBase,
+    title: 'Atelier 801 - Launcher',
+    icon: path.join(__dirname, 'images', 'icon-ext.png'),
+    webContents: {
+      devTools: false
+    }
+  }, path.join('file://', __dirname, 'main.html'));
+
+  win && win.close();
+  win = newWin;
+  currentGame = null;
+}
+
+try {
+  env.verbose && console.log('Loading libpepflashplayer');
+  flashVersion = flash.loadManifest(flashPath).version; // throws an error if flashPath does not exist
+  app.commandLine.appendSwitch('ppapi-flash-path', path.join(flashPath, 'libpepflashplayer.so'));
+  app.commandLine.appendSwitch('ppapi-flash-version', flashVersion);
+  hasFlash = true;
+} catch (err) {
+  if (env.verbose) {
+    console.error('Could not load libpepflashplayer');
+    console.error(err);
+    console.log('Trying to install lilpepflashplayer from Adobe\'s website');
+  }
+  flash.update(flashVersion, flashPath)
+    .then(() => {
+      env.verbose && console.log('Installed lilpepflashplayer successfully');
+      dialog.showMessageBox({
+        title: 'FlashPlayer installed',
+        message: 'Please restart the application',
+        type: 'info',
+        buttons: ['OK']
+      }, () => app.quit());
+    })
+    .catch((err) => {
+      if (env.verbose) {
+        console.error('Could not install lilpepflashplayer');
+        console.error(err);
+      }
+      dialog.showMessageBox({
+        title: 'Error',
+        message: 'Could not reinstall FlashPlayer :(',
+        type: 'error',
+        buttons: ['Close']
+      }, () => app.quit());
+    })
+}
+
+// Starts Application
+app.once('ready', () => {
+  if (!hasFlash) return;
+  env.verbose && console.log('Application is ready');
+  setupShortcuts();
+  openMain();
+});
+
+app.on('window-all-closed', () => (process.platform != 'darwin' && app.quit()));
+app.on('will-quit', () => globalShortcut.unregisterAll());
+
+ipcMain.on('update-flash', (e) => {
+  env.verbose && console.log('Trying to update libpepflashplayer (required by user)');
+  e.sender.send('update-flash-begin');
+
+  flash.update(flashVersion, flashPath)
+    .then(code => e.sender.send('update-flash-end', code))
+    .catch((err) => {
+      if (env.verbose) {
+        console.error('Could not install libpepflashplayer (required by user');
+        console.error(err);
+      }
+      e.sender.send('update-flash-error')
+    })
+    ;
+});
+
+ipcMain.on('open-game', (_, id) => openGame(id));
---
+++ src/modules/flash.js
@@ -0,0 +1,93 @@
+const fetch = require('node-fetch');
+const download = require('download');
+const tar = require('tar');
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+const rimraf = require('rimraf');
+
+function loadFlashManifest(flashPath) {
+  env.verbose && console.log('Reading libpepflashplayer manifest from `%s`', flashPath);
+  return JSON.parse(fs.readFileSync(path.join(flashPath, 'manifest.json'), 'utf-8'));
+}
+
+const toInt = e => parseInt(e);
+
+function compareVersions(a, b) {
+  a = a.split('.').map(toInt);
+  b = b.split('.').map(toInt);
+  const min = Math.min(a.length, b.length);
+
+  for (let i = 0; i < min; i++) {
+    if (a[i] < b[i]) // outdated
+      return -1;
+    else if (a[i] === b[i]) // updated
+      continue;
+    else
+      return 1;
+  }
+  return 0;
+}
+
+let isUpdating = false;
+
+function mktempdir() {
+  env.verbose && console.log('Creating temp directory');
+  return new Promise((resolve, reject) => {
+    fs.mkdtemp(path.join(os.tmpdir(), 'temp-standalone-'), (err, folder) => (err ? reject(err) : resolve(folder)));
+  });
+}
+
+function rmdir(dir) {
+  env.verbose && console.log('Removing directory (%s)', dir);
+  return new Promise((resolve, reject) => rimraf(dir, (err) => (err ? reject(err) : resolve())));
+}
+
+function rename(oldPath, newPath) {
+  env.verbose && console.log('Renaming file `%s` to `%s`', oldPath, newPath);
+  return new Promise((resolve, reject) => fs.rename(oldPath, newPath, err => err ? reject(err) : resolve()));
+}
+
+function downloadUpdate(url, folder) {
+  env.verbose && console.log('Download libpepflashplayer update to `%s`', folder);
+  return new Promise((resolve, reject) => {
+    download(url).pipe(tar.extract({
+      cwd: folder
+    }))
+      .on('finish', () => resolve())
+      .on('error', err => reject(err))
+      ;
+  });
+}
+
+async function updateFlash(currentVersion, flashPath) {
+  if (isUpdating)
+    return;
+
+  env.verbose && console.log('Checking for libpepflashplayer update');
+  
+  isUpdating = true;
+
+  const latestData = await fetch(`https://get.adobe.com/flashplayer/webservices/json/?platform_type=Linux&platform_dist=tar&platform_arch=${(process.arch === 'x64') ? 'x86-64' : 'x86-32'}&browser_arch=&browser_type=&browser_vers=&browser_dist=Chrome&eventname=flashplayerotherversions`)
+    .then(res => res.json())
+    .then(res => res.find(dist => dist.installer_description === 'DOWNLOADCENTER_FLASH_PLAYER_LINUX_TAR_GZ'));
+
+  if (!('Version' in latestData)) {
+    throw 'Missing `Version` field';
+  }
+
+  const res = compareVersions(currentVersion, latestData.Version);
+  if (res === -1) {
+    const folder = await mktempdir();
+  
+    await downloadUpdate(latestData.download_url, folder);
+    await rmdir(flashPath);
+    await rename(folder, flashPath);
+  }
+
+  isUpdating = false;
+  return res;
+}
+
+module.exports.update = updateFlash;
+module.exports.loadManifest = loadFlashManifest;
